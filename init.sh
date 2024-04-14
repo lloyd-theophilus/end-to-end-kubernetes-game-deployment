fda64f95667480a2f7b1dae8821b8444146015a4
Lloyd <osabuteylloyd@gmail.com>
2024-04-14 20:42:40 +0000

    updated jenkins file

--- init.sh
+++ init.sh
@@ -1,2 +1,4 @@
-#eksctl create cluster --name Three-Tier-K8s-game-cluster --region eu-west-2 --node-type t2.medium --nodes-min 2 --nodes-max 2
-#aws eks update-kubeconfig --name Three-Tier-K8s-game-Cluster --region eu-west-2 
+#eksctl create cluster --name three-Tier-K8s-game-cluster --region eu-west-2 --node-type t2.medium --nodes-min 2 --nodes-max 2
+#aws eks --region eu-west-2 update-kubeconfig --name three-Tier-K8s-game-Cluster 
+#aws eks describe-cluster --name three-Tier-K8s-game-cluster
+#To find kubeconfig, check cluster directory on the commandline eg: /Users/user/.kube/config
