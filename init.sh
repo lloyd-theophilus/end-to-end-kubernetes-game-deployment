76c46d7da60541e43b3333443fac0ca68aa6081f
Lloyd <osabuteylloyd@gmail.com>
2024-04-14 21:49:20 +0000

    updated jenkins file

--- init.sh
+++ init.sh
@@ -2,3 +2,4 @@
 #aws eks --region eu-west-2 update-kubeconfig --name three-Tier-K8s-game-Cluster 
 #aws eks describe-cluster --name three-Tier-K8s-game-cluster
 #To find kubeconfig, check cluster directory on the commandline eg: /Users/user/.kube/config
+#Your current user or role does not have access to Kubernetes objects on this EKS cluster (kubectl edit configmap aws-auth -n kube-system, mapUsers: "- groups: \n  - system:masters\n  userarn: arn:aws:iam::671177010163:root\n")
